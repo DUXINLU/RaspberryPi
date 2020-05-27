7dbdc90b1e0aee138e55556d8e31a5773329d418
DUXINLU <duxinlu81@live.com>
2020-05-27 18:12:00 +0800

    '.'

--- OLED/iron_man.py
+++ OLED/iron_man.py
@@ -1,47 +1,69 @@
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306
-from PIL import Image,ImageDraw
+from PIL import Image, ImageDraw
 import numpy as np
 from time import sleep
 
-#oled = Image.open('OLED.png')
+logo_path = 'logo.png'
 
-logo = Image.open('logo.png')
-logo = logo.resize((40, 40))
-logo = logo.convert('L')
 
-logo_np = np.array(logo)
-for i in range(len(logo_np)):
-    for j in range(len(logo_np[i])):
-        if logo_np[i][j] < 100:
-            logo_np[i][j] = 0
+class OLED:
+    def __init__(self):
+        serial = i2c(port=1, address=0x3C)
+        self.device = ssd1306(serial)
 
-logo = Image.fromarray(logo_np)
-logo.convert('1')
+    def Animation(self, start, end):
 
-serial = i2c(port=1, address=0x3C)
-device = ssd1306(serial)
+    def welcome(self):
+        with canvas(self.device) as draw:
+            draw.text((15, 30), 'Welcome home Sir.', fill='white')
+        sleep(3)
+
+    def show_logo(self):
+        # 初始化
+        logo = Image.open(logo_path)
+        logo = logo.resize((40, 40))
+        logo = logo.convert('L')
+        # 切图
+        logo_np = np.array(logo)
+        for i in range(len(logo_np)):
+            for j in range(len(logo_np[i])):
+                if logo_np[i][j] < 100:
+                    logo_np[i][j] = 0
+        # 黑白化
+        logo = Image.fromarray(logo_np)
+        self.logo = logo.convert('1')
+
+        with canvas(self.device) as draw:
+            draw.bitmap((44, 15), logo, fill='white')
+        sleep(3)
+
+        # logo移动
+        logox, logoy = 44, 15
+        logo_size = 40
+        for i in range(5):
+
 
 def init():
-    
+
 
 def show():
     # welcome text
     with canvas(device) as draw:
-        draw.text((15,30),'welcome home Sir',fill='white')
+        draw.text((15, 30), 'welcome home Sir.', fill='white')
     sleep(3)
-    
+
     # show logo
     with canvas(device) as draw:
         draw.bitmap((44, 15), logo, fill='white')
     sleep(1)
-    
-    logo=logo.resize((20,20))
+
+    logo = logo.resize((20, 20))
     with canvas(device) as draw:
-        
         draw.bitmap((0, 0), logo, fill='white')
     sleep(3)
-        
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     show()
---
+++ OLED/pil_test.py
@@ -0,0 +1,41 @@
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306
+from PIL import Image, ImageDraw
+import numpy as np
+from time import sleep
+
+oled = Image.open('OLED.png')
+serial = i2c(port=1, address=0x3C)
+device = ssd1306(serial)
+
+logo = Image.open('head.png')
+logo = logo.resize((40, 40))
+logo = logo.convert('L')
+
+logo_np = np.array(logo)
+for i in range(len(logo_np)):
+    for j in range(len(logo_np[i])):
+        if logo_np[i][j] < 100:
+            logo_np[i][j] = 0
+
+logo = Image.fromarray(logo_np)
+logo.convert('1')
+
+logo_np = np.array(logo)
+for i in range(len(logo_np)):
+    for j in range(len(logo_np[i])):
+        print(logo_np[i][j])
+
+        if logo_np[i][j] >= 200:
+            logo_np[i][j] = 255
+            continue
+        if logo_np[i][j] < 200:
+            logo_np[i][j] = 0
+            continue
+
+logo = Image.fromarray(logo_np)
+
+with canvas(device) as draw:
+    draw.bitmap((44, 15), logo, fill='white')
+    draw.bitmap((0, 0), logo.resize((20, 20)), fill='white')
