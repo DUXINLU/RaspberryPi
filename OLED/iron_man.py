7b79cd247e9668533d7d938b01ecc15f5ff9c8b2
DUXINLU <duxinlu81@live.com>
2020-05-27 11:37:26 +0800

    logo ok

--- OLED/iron_man.py
+++ OLED/iron_man.py
@@ -1,13 +1,23 @@
 from PIL import ImageDraw, Image
+import numpy as np
 from time import sleep
 
 oled = Image.open('OLED.png')
 
 logo = Image.open('logo.png')
-logo = logo.resize((64, 64))
+logo = logo.resize((50, 50))
 logo = logo.convert('L')
 
+logo_np = np.array(logo)
+for i in range(len(logo_np)):
+    for j in range(len(logo_np[i])):
+        if logo_np[i][j] < 100:
+            logo_np[i][j] = 0
+
+logo = Image.fromarray(logo_np)
+logo.convert('1')
+
 draw = ImageDraw.Draw(oled)
-draw.bitmap((34, 0), logo, fill='white')
+draw.bitmap((39, 7), logo, fill='white')
 
 oled.show()
